6adf2f05e8dc66d01aacd096a8d0d05dcb476d2e
aqibfarz <aqibmuqaffi.h@gmail.com>
2022-04-19 16:44:22 +0700

    inisialisasi project

---
+++ Karyawan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ResponsiPemrograman4269
+{
+    class Karyawan
+    {
+        public string Nama { get; set; }
+        public int Nik { get; set; }
+        public int GajiBulanan { get; set; }
+
+        public Karyawan(string nama, int nik, int gaji) 
+        {
+            Nama = nama;
+            Nik = nik;
+            GajiBulanan = gaji;
+        }
+        public void Data()
+        {
+            Console.WriteLine("{0} {1}      {2}", Nik, Nama, GajiBulanan); 
+        }
+        public void Tambah()
+        {
+            int tambah;
+
+            tambah = GajiBulanan * 110;
+            GajiBulanan = tambah / 100;
+
+            Console.WriteLine("asyik naik gaji 10%");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ResponsiPemrograman4269
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan1 = new Karyawan("Paijo", 190302123, 4000000);
+            Karyawan karyawan2 = new Karyawan("Jono", 190302124, 2000000);
+
+            Console.WriteLine("No Nik/Nama      Gaji Bulanan");
+            Console.WriteLine("-----------------------------");
+            Console.Write("1 "); karyawan1.Data();
+            Console.Write("2 "); karyawan2.Data();
+            Console.WriteLine();
+
+            karyawan1.Tambah();
+            karyawan2.Tambah();
+
+            Console.WriteLine();
+            Console.WriteLine("No Nik/Nama      Gaji Bulanan");
+            Console.WriteLine("-----------------------------");
+            Console.Write("1 "); karyawan1.Data();
+            Console.Write("2 "); karyawan2.Data();
+        }
+    }
+}
